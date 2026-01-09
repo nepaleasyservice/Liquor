40bcd357490d0d1e102d4bd151c3607abdd8911a
basantbht <bhattbasant012@gmail.com>
2026-01-09 14:29:59 +0545

    authcontrolelr cookie options added path /

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -7,6 +7,7 @@ const adminCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
