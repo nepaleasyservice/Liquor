319c803c6e1b7b789c4378a40add311962a65ffa
easyservice <nepaleasyservice@gmail.com>
2026-01-09 12:19:23 +0545

    frontend url fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "https://liquor-beta.vercel.app"],
+    origin: [process.env.FRONTEND_URL || "https://liquorfrontend.vercel.app"],
     credentials: true,
   })
 );
